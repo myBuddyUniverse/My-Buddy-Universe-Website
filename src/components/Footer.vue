76c35661d134fda13e13a6f20f4cd9afe7c2814d
lad475 <lad475@nyu.edu>
2019-01-06 22:35:41 -0500

    Adds in global sass files

---
+++ src/components/Footer.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="footer">
+
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator';
+
+@Component
+export default class Footer extends Vue {
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+#footer {
+
+}
+</style>
+
---
+++ src/components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="header">
+
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator';
+
+@Component
+export default class Header extends Vue {
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+#header {
+
+}
+</style>
